3b5a8008901ff66a657ab223fd7b1b6ab91e7e91
Muhamad Rifky <muhamad210501@gmail.com>
2025-08-31 05:22:55 +0700

    Remove simple-password-encrypt.js file, which contained password hashing logic using bcrypt.

--- simple-password-encrypt.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const rawPass = "admin123";
-
-// Hash function
-const result = await bcrypt.hash(rawPass, 10);
-
-console.log("Password asli:", rawPass);
-console.log("Password terenkripsi:", result);
